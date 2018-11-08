f36c482305b5e47171667f5f3c50a1dce72dec5c
psc-kamei <info.pscreator@gmail.com>
2018-11-08 13:11:20 +0900

    init

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema');
+const app = express();
+
+app.use('/graphql', expressGraphQL({
+   schema,
+   graphiql: true
+}))
+app.listen(4000, () => {
+   console.log('Listening...')
+})
